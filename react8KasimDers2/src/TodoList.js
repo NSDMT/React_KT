957b897e61bda9ebb831c3dfbfecd3bee4a929d4
NSDMT <114123942+NSDMT@users.noreply.github.com>
2023-11-08 12:21:49 +0300

    kasim8ders2
    boş bir array içine oncgange değişen value ları puslayarak maple diziyi listeledim fonksiyonu butona onclick olarak atadım

---
+++ react8KasimDers2/src/TodoList.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+function Todo() {
+  const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState('');
+
+  const addTodo = () => {
+    if (task) {
+      setTodos([...todos, task]);
+      setTask('');
+    }
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => removeTodo(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Todo;
